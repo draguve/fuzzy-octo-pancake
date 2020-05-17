fb74b3b189b168912f2e0c80c1a8fe425ee1919d
Draguve <draguve@gmail.com>
2020-05-18 01:54:11 +0530

    admin now using async/await

--- Routes/admin.js
+++ Routes/admin.js
@@ -33,37 +33,36 @@ router.post(
 			.not()
 			.isEmpty(),
 	],
-	function (req, res) {
+	async (req, res) => {
 		if (validateToast(req)) {
 			return res.redirect(req.baseUrl + "/login");
 		}
-		Admin.findOne({ email: req.body.email })
-			.then((doc) => {
-				if (doc) {
-					if (doc.validPassword(req.body.password)) {
-						if (
-							req.session.userType &&
-							req.session.email == doc.email
-						) {
-							req.session.userType.push(adminType);
-						} else {
-							req.session.userType = [adminType];
-						}
-						req.session.email = doc.email;
-
-						res.redirect(req.baseUrl + "/");
+		try {
+			var doc = await Admin.findOne({ email: req.body.email });
+			if (doc) {
+				if (doc.validPassword(req.body.password)) {
+					if (
+						req.session.userType &&
+						req.session.email == doc.email
+					) {
+						req.session.userType.push(adminType);
 					} else {
-						addToast("Incorrect Password", req);
-						res.render("./Admin/login.html");
+						req.session.userType = [adminType];
 					}
+					req.session.email = doc.email;
+
+					res.redirect(req.baseUrl + "/");
 				} else {
-					addToast("User with email id doesn't exist", req);
+					addToast("Incorrect Password", req);
 					res.render("./Admin/login.html");
 				}
-			})
-			.catch((err) => {
-				console.error(err);
-			});
+			} else {
+				addToast("User with email id doesn't exist", req);
+				res.render("./Admin/login.html");
+			}
+		} catch (err) {
+			return res.send(err);
+		}
 	}
 );
 
@@ -92,37 +91,30 @@ router.post(
 			.not()
 			.isEmpty(),
 	],
-	function (req, res) {
+	async (req, res) => {
 		if (validateToast(req)) {
 			return res.redirect(req.baseUrl + "/signup");
 		}
-		Admin.find({ email: req.body.email })
-			.then((doc) => {
-				if (doc.length > 0) {
-					var refilData = {
-						hospitalName: req.body.hospitalName,
-					};
-					addToast("Email ID already in use", req);
-					return res.render("./Admin/signup.html", refilData);
-				}
-				let admin = new Admin({
-					email: req.body.email,
-					hospName: req.body.hospitalName,
-				});
-				admin.setPassword(req.body.password);
-				admin
-					.save()
-					.then(() => {
-						addToast("New Hospital Added", req);
-						res.redirect(req.baseUrl + "/login");
-					})
-					.catch((err) => {
-						console.error(err);
-					});
-			})
-			.catch((err) => {
-				console.error(err);
+		try {
+			var doc = await Admin.find({ email: req.body.email });
+			if (doc.length > 0) {
+				var refilData = {
+					hospitalName: req.body.hospitalName,
+				};
+				addToast("Email ID already in use", req);
+				return res.render("./Admin/signup.html", refilData);
+			}
+			let admin = new Admin({
+				email: req.body.email,
+				hospName: req.body.hospitalName,
 			});
+			admin.setPassword(req.body.password);
+			await admin.save();
+			addToast("New Hospital Added", req);
+			res.redirect(req.baseUrl + "/login");
+		} catch (err) {
+			return res.send(err);
+		}
 	}
 );
 
