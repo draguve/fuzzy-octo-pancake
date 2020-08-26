6c69c276b82d384d10e063c88abf716674ab2899
draguve <draguve@gmail.com>
2020-08-26 18:55:53 +0530

    added langs

--- Nunjucks/requestInjector.js
+++ Nunjucks/requestInjector.js
@@ -1,4 +1,12 @@
+const translate = require('@k3rn31p4nic/google-translate-api');
+let languages = JSON.parse(JSON.stringify(translate.languages));
+delete languages.auto;
+
+
 module.exports = function (req, res, next) {
 	res.locals.req = req;
+
+	//also injecting languages
+	res.locals.allLangs=languages;
 	next();
 };
